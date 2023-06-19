ba29d6c53979b743747822ecfbe1f7863d56c5a8
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2023-06-20 03:21:51 +0700

    Add Feature Expense

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="container">
-    <ul>
-      <li><router-link :to="{ name: 'home' }">Home</router-link></li>
-      <li><router-link :to="{ name: 'income' }">Pemasukan</router-link></li>
-      <li><router-link :to="{ name: 'expense' }">Pengeluaran</router-link></li>
-    </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+      <div class="ps-5 py-2">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link :to="{name : 'home'}" class="nav-link" exact-active-class="active">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{name: 'income'}" class="nav-link" exact-active-class="active">Income</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{name: 'expense'}" class="nav-link" exact-active-class="active">Expense</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
-    <router-view></router-view>
-  </div>
+    <main class="vh-100 m-5">
+      <router-view></router-view>
+    </main>
 </template>
