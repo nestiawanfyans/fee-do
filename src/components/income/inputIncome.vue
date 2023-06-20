984220cc8eb782d98c630bd299eb1f2455a2af39
nestiawanfyans <fyandev@gmail.com>
2023-06-20 23:10:14 +0700

    feature income

--- src/components/income/inputIncome.vue
+++ src/components/income/inputIncome.vue
@@ -1,43 +1,88 @@
-<script setup>
-
-</script>
 
 <template>
-    <div class="bg-white shadow-sm rounded p-2">
-      <div class="container me-2 mt-2">
-        <h5 class="text-body">Tambah Pemasukan</h5><br>
+  <div class="bg-white shadow-sm rounded p-2">
+    <div class="container me-2 mt-2">
+      <h5 class="text-body">Tambah Pemasukan</h5>
+      <br />
 
-        <div class="from row">
-
-          <div class="col-md-6">
-            <!--Judul-->
-            <div class="input-group mb-3">
-              <span class="input-group-text" id="basic-addon1">Judul</span>
-              <input type="text" class="form-control" placeholder="Jumlah Pemasukan">
-            </div>
-          </div>
-
-          <div class="col-md-6">
-            <!--jumlah Pemasukan-->
-            <div class="input-group mb-3">
-              <span class="input-group-text" id="basic-addon1">Rp.</span>
-              <input type="text" class="form-control" placeholder="Jumlah Pemasukan">
-            </div>
+      <div class="from row">
+        <div class="col-md-6">
+          <!--Judul-->
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Judul</span>
+            <input type="text" class="form-control" v-model="titleIncome" placeholder="Judul Pemasukan" />
           </div>
 
           <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Tag</span>
-            <input type="text" class="form-control" placeholder="Jumlah Pemasukan">
+            <select class="form-select" v-model="category">
+              <option selected disabled>Kategori Pemasukan</option>
+              <option v-for="categori in categories" :key="categori.id">{{ categori.name }}</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <!--jumlah Pemasukan-->
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Rp.</span>
+            <input type="number" class="form-control" v-model="money" placeholder="Jumlah Pemasukan" />
           </div>
 
-          <div class="d-grid gap-2 d-md-flex justify-content-md-end mb-2">
-            <button class="btn btn-primary">Tambah Pemasukan</button>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Tanggal</span>
+            <input type="date" class="form-control" v-model="dateNow" placeholder="Jumlah Pemasukan" />
           </div>
+        </div>
+
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end mb-2">
+          <button class="btn btn-primary" @click="createIncome">Tambah Pemasukan</button>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
-<style scoped>
+<script>
+import Swal from "sweetalert2";
 
-</style>
+export default {
+  props: ['categories'],
+  emits: ['create-income'],
+  data() {
+    return {
+      titleIncome : '',
+      money: '',
+      category: 'Kategori Pemasukan',
+      dateNow: new Date().toISOString().substring(0, 10),
+      toast: Swal.mixin({
+        toast: true,
+        position: "top-right",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+      }),
+    }
+  },
+  methods: {
+    createIncome() {
+      if(this.titleIncome == null || this.money == null || this.category == 'Kategori Pemasukan') {
+        this.toast.fire({
+          icon: "error",
+          title: "Gagal Tambah Pemasukan"
+        })
+      } else {
+        this.$emit("create-income", {
+          id: Math.floor(Math.random() * 1000),
+          title: this.titleIncome,
+          money: this.money,
+          created_at: this.dateNow,
+          updated_at: null,
+          category: this.category
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped></style>
--- src/views/IncomeView.vue
+++ src/views/IncomeView.vue
@@ -9,31 +9,72 @@
   <p>Record Data Pemasukan kamu, agar uang kamu hemat!!</p><br>
 
   <div class="container">
-    <InputIncome /> <br>
-    <ListIncome /> <br>
+    <InputIncome :categories="categories" @create-income="handleCreateIncome" /> <br>
+    <ListIncome :incomes="incomes" :categories="categories" @delete-income="handelDeleteIncome" @update-income="handleUpdateIncome"/> <br>
   </div>
 
 </template>
 
 <script>
+  import Swal from "sweetalert2";
+
   export default {
-    // components: ,
+    components: ['InputIncome', 'ListIncome'],
     data() {
       return {
-        income: [
+        categories: [
+          {id: Math.floor(Math.random() * 1000), name: "Gaji"},
+          {id: Math.floor(Math.random() * 1000), name: "Uang Jajan"},
+          {id: Math.floor(Math.random() * 1000), name: "Pemberian Orang Tua"},
+          {id: Math.floor(Math.random() * 1000), name: "Jual Barang"},
+          {id: Math.floor(Math.random() * 1000), name: "Pay Latter"},
+        ],
+        incomes: [
           {
-            id : 1312312123,
+            id : Math.floor(Math.random() * 1000),
             title: "project website",
             money: 123000,
             created_at: "2023-02-23",
             updated_at: "2023-02-23",
             category: "Gaji"
           }
-        ]
+        ],
+        toast: Swal.mixin({
+          toast: true,
+          position: "top-right",
+          showConfirmButton: false,
+          timer: 2000,
+          timerProgressBar: true,
+        }),
       }
     },
     methods: {
+      handleCreateIncome(incomes) {
+        this.incomes = [...this.incomes, incomes];
 
+        return this.toast.fire({
+          icon: "success",
+          title: "Berhasil Menambahkan Pemasukan"
+        })
+      },
+
+      handelDeleteIncome(index){
+        this.incomes.splice(index, 1)
+
+        return this.toast.fire({
+          icon: "success",
+          title: "Berhasil Menghapus Pemasukan"
+        })
+      },
+
+      handleUpdateIncome(dataUpdate) {
+        this.incomes[dataUpdate.updateIndex] = dataUpdate
+
+        return this.toast.fire({
+          icon: "success",
+          title: "Berhasil Merubah Pemasukan"
+        })
+      }
     }
   }
 
